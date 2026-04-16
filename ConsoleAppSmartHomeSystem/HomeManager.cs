3655a4c3c80d012f533940bd94f74cdfa120c6bd
JakobCoding14 <manu.henni83@gmail.com>
2026-04-16 08:42:52 +0200

    another test after git crash

--- ConsoleAppSmartHomeSystem/HomeManager.cs
+++ ConsoleAppSmartHomeSystem/HomeManager.cs
@@ -16,7 +16,6 @@ public class HomeManager
     public int CountWatts()
     {
         var totalWatts = DeviceList.Where(d => d.IsOn).Sum(d => d.Watts);
-        int test = 0;
         return totalWatts;
         
     }
